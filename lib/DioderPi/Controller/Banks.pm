8842d83b9bc1abf2fe5553fd73f16c40cf09a6c0
chandwer <chris.handwerker@gmail.com>
2016-11-18 23:19:04 +0000

    get and put actions for /bank/{name}

--- lib/DioderPi/Controller/Banks.pm
+++ lib/DioderPi/Controller/Banks.pm
@@ -8,4 +8,51 @@ sub list {
 	$c->render( openapi => \@banks );
 }
 
+sub get_bank {
+	my ($c) = @_;
+
+	$c->openapi->valid_input or return;
+
+	if( my $bank = $c->_bank ) {
+		$c->render( openapi => $bank );
+	} else {
+		$c->render( $c->_bank_not_found );
+	}
+}
+
+sub update_bank {
+	my ($c) = @_;
+
+	$c->openapi->valid_input or return;
+
+	if( my $bank = $c->_bank ) {
+		$bank->rgb( $c->req->json );
+		$c->render( openapi => $bank );
+	} else {
+		$c->render( $c->_bank_not_found );
+	}
+}
+
+sub _bank {
+	my ($c) = @_;
+
+	return $c->banks->{ $c->param('name') };
+}
+sub _bank_not_found {
+	my ($c) = @_;
+
+	return (
+		status  => 404,
+		openapi => {
+			errors => [
+				{
+					status => "404",
+					title  => "Invalid Bank",
+					detail => "Bank '" . $c->param("name") . "' is not defined."
+				}
+			]
+		}
+	);
+}
+
 1;
